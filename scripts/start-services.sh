3e26699a34809bbc80f000896cc87d731d3a1883
KittyKatt <kittykatt@kittykatt.co>
2020-09-28 12:52:04 -0500

    Startup succeeded of both services, doing some cleanup then local testing of actual proxy

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -85,35 +85,35 @@ find_proxy_conf() {
     fi
 }
 set_proxy_port() {
-    expr $1 + 0 1>/dev/null 2>&1
+    expr ${1} + 0 1>/dev/null 2>&1
     status=$?
     if test ${status} -gt 1
     then
-        echo "[!] Port [$1]: Not a number." >&2; exit 1
+        echo "[!] Port [${1}]: Not a number." >&2; exit 1
     fi
 
     # Port: Specify the port which tinyproxy will listen on.    Please note
     # that should you choose to run on a port lower than 1024 you will need
     # to start tinyproxy using root.
 
-    if test $1 -lt 1024
+    if test ${1} -lt 1024
     then
-        echo "[!] tinyproxy: $1 is lower than 1024. Ports below 1024 are not permitted.";
+        echo "[!] tinyproxy: ${1} is lower than 1024. Ports below 1024 are not permitted.";
         exit 1
     fi
 
-    echo "[#] Setting tinyproxy port to $1.";
-    sed -i -e"s,^Port .*,Port $1," $2
+    echo "[#] Setting tinyproxy port to ${1}.";
+    sed -i -e"s,^Port .*,Port ${1}," ${2}
 }
 
 set_proxy_authentication() {
     echo "[#] Setting tinyproxy basic auth.";
-    echo "BasicAuth $1 $2" >> $3
+    echo "BasicAuth ${1} ${2}" >> ${3}
 }
 
 set_proxy_bind_ip() {
-    echo "[#] Setting bind IP to ${PROXY_BIND}."
-    sed -i -e "s,^#Bind .*,Bind $1," $2
+    echo "[#] Set proxy bind IP to ${WG_IP}."
+    sed -i -e "s,^#Bind .*,Bind ${1}," ${2}
 }
 
 if [[ "${WEBPROXY_ENABLED}" = "true" ]]; then
@@ -125,7 +125,7 @@ if [[ "${WEBPROXY_ENABLED}" = "true" ]]; then
     if [[ ! -z "${WEBPROXY_PORT}" ]]; then
         set_proxy_port ${WEBPROXY_PORT} ${PROXY_CONF}
     fi
-    echo "[#] Set proxy bind IP to $WG_IP."
+    
     set_proxy_bind_ip ${WG_IP} ${PROXY_CONF}
 
     if [[ ! -z "${WEBPROXY_USERNAME}" ]] && [[ ! -z "${WEBPROXY_PASSWORD}" ]]; then
