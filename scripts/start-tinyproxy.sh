dc7acb5fcfa814199898eac8c259245db1f7b848
KittyKatt <kittykatt@kittykatt.co>
2020-09-28 10:51:33 -0500

    Created start-tinyproxy.sh logic

--- scripts/start-tinyproxy.sh
+++ scripts/start-tinyproxy.sh
@@ -1,3 +1,77 @@
 #!/bin/bash
 
-echo "[$(date)]  TinyProxy not ready yet!"
+# Source our persisted env variables from container startup
+. /etc/transmission/environment-variables.sh
+
+WEBPROXY_BIND_IP="${1}"
+
+find_proxy_conf() {
+    if [[ -f /etc/tinyproxy.conf ]]; then
+        PROXY_CONF='/etc/tinyproxy.conf'
+    elif [[ -f /etc/tinyproxy/tinyproxy.conf ]]; then
+        PROXY_CONF='/etc/tinyproxy/tinyproxy.conf'
+    else
+        echo "[!] ERROR: Could not find tinyproxy config file. Exiting..."
+        exit 1
+    fi
+}
+
+set_port() {
+    expr $1 + 0 1>/dev/null 2>&1
+    status=$?
+    if test ${status} -gt 1
+    then
+        echo "[!] Port [$1]: Not a number." >&2; exit 1
+    fi
+
+    # Port: Specify the port which tinyproxy will listen on.    Please note
+    # that should you choose to run on a port lower than 1024 you will need
+    # to start tinyproxy using root.
+
+    if test $1 -lt 1024
+    then
+        echo "[!] tinyproxy: $1 is lower than 1024. Ports below 1024 are not permitted.";
+        exit 1
+    fi
+
+    echo "[#] Setting tinyproxy port to $1.";
+    sed -i -e"s,^Port .*,Port $1," $2
+}
+
+set_authentication() {
+    echo "[#] Setting tinyproxy basic auth.";
+    echo "BasicAuth $1 $2" >> $3
+}
+
+set_bind_ip() {
+    echo "[#] Setting bind IP to ${PROXY_BIND}."
+    sed -i -e "s,^Bind .*,Bind $1," $2
+}
+
+if [[ "${WEBPROXY_ENABLED}" = "true" ]]; then
+
+    echo "[$(date)]  Initializing Tinyproxy."
+
+    find_proxy_conf
+    echo "[#] Found config file $PROXY_CONF, updating settings."
+
+    set_port ${WEBPROXY_PORT} ${PROXY_CONF}
+    set_bind_ip ${WEBPROXY_BIND_IP} ${PROXY_CONF}
+
+    if [[ ! -z "${WEBPROXY_USERNAME}" ]] && [[ ! -z "${WEBPROXY_PASSWORD}" ]]; then
+        set_authentication ${WEBPROXY_USERNAME} ${WEBPROXY_PASSWORD} ${PROXY_CONF}
+    fi
+
+    # Allow all clients
+    sed -i -e"s/^Allow /#Allow /" ${PROXY_CONF}
+
+    # Disable Via Header for privacy (leaks that you're using a proxy)
+    sed -i -e "s/#DisableViaHeader/DisableViaHeader/" ${PROXY_CONF}
+
+    # Lower log level for privacy (writes dns names by default)
+    sed -i -e "s/LogLevel Info/LogLevel Critical/" ${PROXY_CONF}
+
+    /etc/init.d/tinyproxy start
+    echo "[#] Tinyproxy startup script complete."
+
+fi
